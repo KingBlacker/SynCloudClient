33cd11c39db9598afd94d4df23e4d598feb1750d
KingBlacker <971495737@qq.com>
2015-08-03 13:21:19 +0800

    Added by KingBlacker:some menu
    The menu of
    Login,Regist,MainMenu,Setting,SyncedFile,SyncUserMenu,FileChoose.

---
+++ Menu/src/npt/SynClound/Menu/Register.java
@@ -0,0 +1,65 @@
+package npt.SynClound.Menu;
+
+import javax.swing.*;
+
+import npt.SynClound.Menu.Login.UserRegisterListener;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Register extends JFrame{
+		JTextField text01;
+		JPasswordField text02,text03;
+		JPanel panel01,panel02,panel03,panel04;
+		JLabel label01,label02,label03;
+		JButton button;
+		public Register(){
+			text01 = new JTextField(10);
+			text02 = new JPasswordField(10);
+			text03 = new JPasswordField(10);
+			label01 = new JLabel("用户名    ：");
+			label02 = new JLabel("密码         ：");
+			label03 = new JLabel("确认密码：");
+			button = new JButton("确认");
+			button.addActionListener(new UserRegisterListener());
+			panel01 = new JPanel();
+			panel02 = new JPanel();
+			panel03 = new JPanel();
+			panel04 = new JPanel();
+			
+			this.setLayout(new FlowLayout());
+			
+			panel01.add(label01);
+			panel01.add(text01);
+			
+			panel02.add(label02);
+			panel02.add(text02);
+			
+			panel03.add(label03);
+			panel03.add(text03);
+			
+			panel04.add(button);
+			
+			this.add(panel01);
+			this.add(panel02);
+			this.add(panel03);
+			this.add(panel04);
+			
+			this.setSize(250,250);
+			
+			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			this.setVisible(true);
+			this.setTitle("用户注册");
+		}
+		
+		class UserRegisterListener implements ActionListener{
+			public void actionPerformed(ActionEvent e){
+				if(e.getActionCommand()=="确认"){
+					System.out.println(text01.getText());
+					System.out.println(text02.getText());
+					System.out.println(text03.getText());
+				}
+			}
+		}
+}

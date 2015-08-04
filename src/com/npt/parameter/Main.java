73dc94c93b588ef01e6eb3faaca65440d6146f01
zhpmatrix <zhpmatrix@gmail.com>
2015-08-04 17:35:40 +0800

    Added by zhpmatrix:add the module named Rsync.
    add the  module named Rsync to use java to run the command.

---
+++ src/com/npt/parameter/Main.java
@@ -0,0 +1,30 @@
+package com.npt.parameter;
+
+import java.io.IOException;
+
+/**
+ * example:
+ *        rsync  -av rsync://192.168.0.124:873/doc  /cygdrive/c/sync
+ * 
+ * @author matrix
+ *
+ */
+public class Main {
+	public static void main(String args[]) throws IOException{
+		//get the input parameter
+		String ip = "192.168.0.126";
+		String port = "873";
+		String moduleName = "doc";
+		String dstPath = "c/sync";
+		
+		//get the rsync string
+	    Rsync rsync = new Rsync(ip,port,moduleName,dstPath);
+	    String cmdRsync = rsync.getCmd();
+		rsync.display(cmdRsync);
+		
+		//get the cmd string
+		String cmdStr = "cmd /k "+cmdRsync;
+		rsync.display(cmdStr);
+		Runtime.getRuntime().exec(cmdStr);
+}
+}
---
+++ src/com/npt/parameter/Rsync.java
@@ -0,0 +1,45 @@
+package com.npt.parameter;
+
+/**
+ *use rsync cmd to make the synchronic files
+ *cmd: rsync  -av rsync://192.168.0.124:873/doc  /cygdrive/c/sync
+ *
+ **/
+
+public class Rsync {
+	
+	private String ip;
+	private String port;
+	private String moduleName;
+	/**
+	 * src:the module in the server
+	 * dst:the module in the local machine
+	 */
+	private String dstPath;
+	
+	public Rsync(String ip,String port,String moduleName,String dstPath){
+	       this.ip = ip;
+	       this.port = port;
+	       this.moduleName = moduleName;
+	       this.dstPath = dstPath;
+	}
+	
+	/**
+	 * concat the string of the command
+	 * @return
+	 */
+	public String getCmd(){
+
+		String cmd = "rsync -av rsync://"+this.ip+":"+this.port+"/"+this.moduleName+"	/cygdrive/"+this.dstPath;
+		return cmd;
+	}
+	/**
+	 * display the string 
+	 * @param str
+	 */
+	public void display(String str){
+		System.out.println("---------\n"+str+"\n---------\n");
+	}
+	
+	
+}
